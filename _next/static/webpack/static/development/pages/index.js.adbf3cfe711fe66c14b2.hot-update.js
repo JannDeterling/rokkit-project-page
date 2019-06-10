bb6773d2e0707b99a380645f299840b5ba375c56
Jann Deterling <jann.deterling@smail.th-koeln.de>
2019-06-10 23:01:27 +0200

    Deploy to gh-pages

---
+++ _next/static/webpack/static/development/pages/index.js.adbf3cfe711fe66c14b2.hot-update.js
@@ -0,0 +1,93 @@
+webpackHotUpdate("static\\development\\pages\\index.js",{
+
+/***/ "./components/header/mobile-nav/mobileNav.js":
+/*!***************************************************!*\
+  !*** ./components/header/mobile-nav/mobileNav.js ***!
+  \***************************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _babel_runtime_corejs2_helpers_esm_classCallCheck__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/classCallCheck */ "./node_modules/@babel/runtime-corejs2/helpers/esm/classCallCheck.js");
+/* harmony import */ var _babel_runtime_corejs2_helpers_esm_createClass__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/createClass */ "./node_modules/@babel/runtime-corejs2/helpers/esm/createClass.js");
+/* harmony import */ var _babel_runtime_corejs2_helpers_esm_possibleConstructorReturn__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/possibleConstructorReturn */ "./node_modules/@babel/runtime-corejs2/helpers/esm/possibleConstructorReturn.js");
+/* harmony import */ var _babel_runtime_corejs2_helpers_esm_getPrototypeOf__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/getPrototypeOf */ "./node_modules/@babel/runtime-corejs2/helpers/esm/getPrototypeOf.js");
+/* harmony import */ var _babel_runtime_corejs2_helpers_esm_inherits__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @babel/runtime-corejs2/helpers/esm/inherits */ "./node_modules/@babel/runtime-corejs2/helpers/esm/inherits.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_5___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_5__);
+/* harmony import */ var _menuButton__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./menuButton */ "./components/header/mobile-nav/menuButton.js");
+
+
+
+
+
+var _jsxFileName = "D:\\Jann\\Documents\\UNI-MA\\MasterArbeit\\rokkit.ts\\rokkit.ts-next-app\\components\\header\\mobile-nav\\mobileNav.js";
+
+
+
+var MobileNav =
+/*#__PURE__*/
+function (_React$Component) {
+  Object(_babel_runtime_corejs2_helpers_esm_inherits__WEBPACK_IMPORTED_MODULE_4__["default"])(MobileNav, _React$Component);
+
+  function MobileNav(props) {
+    var _this;
+
+    Object(_babel_runtime_corejs2_helpers_esm_classCallCheck__WEBPACK_IMPORTED_MODULE_0__["default"])(this, MobileNav);
+
+    _this = Object(_babel_runtime_corejs2_helpers_esm_possibleConstructorReturn__WEBPACK_IMPORTED_MODULE_2__["default"])(this, Object(_babel_runtime_corejs2_helpers_esm_getPrototypeOf__WEBPACK_IMPORTED_MODULE_3__["default"])(MobileNav).call(this, props));
+    _this.menu = false;
+    return _this;
+  }
+
+  Object(_babel_runtime_corejs2_helpers_esm_createClass__WEBPACK_IMPORTED_MODULE_1__["default"])(MobileNav, [{
+    key: "toggleMenu",
+    value: function toggleMenu() {
+      this.menu = !this.menu;
+    }
+  }, {
+    key: "render",
+    value: function render() {
+      if (this.menu) {
+        return react__WEBPACK_IMPORTED_MODULE_5__["createElement"]("div", {
+          __source: {
+            fileName: _jsxFileName,
+            lineNumber: 17
+          },
+          __self: this
+        }, react__WEBPACK_IMPORTED_MODULE_5__["createElement"]("h1", {
+          __source: {
+            fileName: _jsxFileName,
+            lineNumber: 18
+          },
+          __self: this
+        }, "Show"));
+      }
+
+      return react__WEBPACK_IMPORTED_MODULE_5__["createElement"]("div", {
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 22
+        },
+        __self: this
+      }, react__WEBPACK_IMPORTED_MODULE_5__["createElement"](_menuButton__WEBPACK_IMPORTED_MODULE_6__["default"], {
+        onClick: this.toggleMenu,
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 23
+        },
+        __self: this
+      }));
+    }
+  }]);
+
+  return MobileNav;
+}(react__WEBPACK_IMPORTED_MODULE_5__["Component"]);
+
+/* harmony default export */ __webpack_exports__["default"] = (MobileNav);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.adbf3cfe711fe66c14b2.hot-update.js.map
---
+++ _next/static/webpack/static/development/pages/index.js.eaba8fa57bef88af39ab.hot-update.js
@@ -0,0 +1,410 @@
+webpackHotUpdate("static\\development\\pages\\index.js",{
+
+/***/ "./components/App.js":
+/*!***************************!*\
+  !*** ./components/App.js ***!
+  \***************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _App_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./App.scss */ "./components/App.scss");
+/* harmony import */ var _App_scss__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(_App_scss__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _header_header__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./header/header */ "./components/header/header.js");
+/* harmony import */ var _background_container_backgroundContainer__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./background-container/backgroundContainer */ "./components/background-container/backgroundContainer.js");
+/* harmony import */ var _background_container_backgroundContainerReverted__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./background-container/backgroundContainerReverted */ "./components/background-container/backgroundContainerReverted.js");
+/* harmony import */ var _content_container_contentContainer__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./content-container/contentContainer */ "./components/content-container/contentContainer.js");
+/* harmony import */ var _footer_Footer__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./footer/Footer */ "./components/footer/Footer.js");
+/* harmony import */ var _node_modules_react_syntax_highlighter__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ../node_modules/react-syntax-highlighter */ "./node_modules/react-syntax-highlighter/dist/esm/index.js");
+/* harmony import */ var _node_modules_react_syntax_highlighter_dist_esm_styles_hljs__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! ../node_modules/react-syntax-highlighter/dist/esm/styles/hljs */ "./node_modules/react-syntax-highlighter/dist/esm/styles/hljs/index.js");
+var _jsxFileName = "D:\\Jann\\Documents\\UNI-MA\\MasterArbeit\\rokkit.ts\\rokkit.ts-next-app\\components\\App.js";
+
+
+
+
+
+
+
+
+
+var sampleControllerCode = "\n import {Controller, Get} from \"rokkit.ts-web\";\n\n @Controller(\"/hello\")\n export class MyController {\n\n   @Get(\"/\")\n   public getHello(): string {\n     return \"Hello World, it's Rokkit.ts!\"\n   }\n }\n";
+var sampleConfiguration = "\n /* config.json -- main config file */\n {\n    \"server\": {\n      \"name\": \"MyRokkit.ts Project\"\n    },\n    environment: {\n      \"active\": \"dev\"\n    }\n }\n \n /* config-dev.json -- config file for development */\n {\n    server: {\n      \"port\": 8080,\n      \"adapter\": \"express\"\n    }\n }\n /* config-prod.json -- config file for production */\n {\n    server: {\n      \"port\": 8080,\n      \"adapter\": \"express\"\n    }\n }\n";
+
+function App() {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "App",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 54
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_header_header__WEBPACK_IMPORTED_MODULE_2__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 55
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_background_container_backgroundContainer__WEBPACK_IMPORTED_MODULE_3__["default"], {
+    highlight: "Rokkit.ts",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 56
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_content_container_contentContainer__WEBPACK_IMPORTED_MODULE_5__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 57
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "attribute-box",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 58
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "attribute-text",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 59
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h2", {
+    className: "text-color-pink",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 60
+    },
+    __self: this
+  }, "Focused"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 61
+    },
+    __self: this
+  }, "Rokkit focuses on major microservices attributes:"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("ul", {
+    className: "highlight-list",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 62
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 63
+    },
+    __self: this
+  }, "Communication"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 64
+    },
+    __self: this
+  }, "Resilience"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 65
+    },
+    __self: this
+  }, "Scalability"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 66
+    },
+    __self: this
+  }, "Performance"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("li", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 67
+    },
+    __self: this
+  }, "Technical independence"))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "attribute-text",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 70
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h2", {
+    className: "text-color-pink",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 71
+    },
+    __self: this
+  }, "Modulized"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 72
+    },
+    __self: this
+  }, "Rokkit.ts is separated into modules to reduce the build size and deliver the perfect application, instead of rely on unused dependencies. Each module of Rokkit.ts can be used independently or combined by the Rokkit Core.", react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("br", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 75
+    },
+    __self: this
+  }), "Rokkits comes with 5 modules:"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    className: "text-color-pink",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 78
+    },
+    __self: this
+  }, "Core,\xA0 Web,\xA0 Messaging,\xA0 Data,\xA0 Logging & Monitoring")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "attribute-text",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 86
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h2", {
+    className: "text-color-pink",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 87
+    },
+    __self: this
+  }, "TypeScript"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 88
+    },
+    __self: this
+  }, "Rokkit.ts is written for TypeScript.", react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("br", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 89
+    },
+    __self: this
+  }), "We use TypeScript to ensure type safeness and empower developers in their application.", react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("br", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 91
+    },
+    __self: this
+  }), "TypeScript enables us to detect types error during development and provides safeness in production.")))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_background_container_backgroundContainerReverted__WEBPACK_IMPORTED_MODULE_4__["default"], {
+    highlight: "Easy to Develop",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 98
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_content_container_contentContainer__WEBPACK_IMPORTED_MODULE_5__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 99
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "attribute-box",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 100
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "attribute-text",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 101
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h2", {
+    className: "text-color-pink",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 102
+    },
+    __self: this
+  }, "Annotation based development"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 103
+    },
+    __self: this
+  }, "Rokkit's features are primary used by annotations.", react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("br", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 104
+    },
+    __self: this
+  }), "Annotations keeps your code clean and deliver maximal functionality!"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_node_modules_react_syntax_highlighter__WEBPACK_IMPORTED_MODULE_7__["default"], {
+    className: "syntax-hl",
+    language: "typescript",
+    style: _node_modules_react_syntax_highlighter_dist_esm_styles_hljs__WEBPACK_IMPORTED_MODULE_8__["dracula"],
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 108
+    },
+    __self: this
+  }, sampleControllerCode)), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "attribute-text",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 113
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h2", {
+    className: "text-color-pink",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 114
+    },
+    __self: this
+  }, "Conditional configuration"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 115
+    },
+    __self: this
+  }, "Rokkit.ts can be configured for multiple environments."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_node_modules_react_syntax_highlighter__WEBPACK_IMPORTED_MODULE_7__["default"], {
+    className: "syntax-hl",
+    language: "json",
+    style: _node_modules_react_syntax_highlighter_dist_esm_styles_hljs__WEBPACK_IMPORTED_MODULE_8__["dracula"],
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 118
+    },
+    __self: this
+  }, sampleConfiguration)))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_background_container_backgroundContainer__WEBPACK_IMPORTED_MODULE_3__["default"], {
+    highlight: "Fast & Resource saving",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 126
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_content_container_contentContainer__WEBPACK_IMPORTED_MODULE_5__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 127
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "attribute-box",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 128
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "attribute-text",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 129
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h2", {
+    className: "text-color-pink",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 130
+    },
+    __self: this
+  }, "Fast start up"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 131
+    },
+    __self: this
+  }, "Microservices need to scale up fast, when necessary due to load peaks or failures of other instances. Rokkit.ts is designed to start up fast and take requests as early as possible. Only needed modules and components will be started by Rokkit.ts.")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "attribute-text",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 137
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h2", {
+    className: "text-color-pink",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 138
+    },
+    __self: this
+  }, "Low memory consumption"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 139
+    },
+    __self: this
+  }, "Rokkit.ts will not consume unnecessary amounts of memory. Rokkit.ts will manage class instances and tries to reuse as many instances as possible. Beside the framework Node.js offers a high-performing runtime with low memory consumption.")))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_background_container_backgroundContainerReverted__WEBPACK_IMPORTED_MODULE_4__["default"], {
+    highlight: "Diverse",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 149
+    },
+    __self: this
+  }), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_content_container_contentContainer__WEBPACK_IMPORTED_MODULE_5__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 150
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "attribute-box",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 151
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "attribute-text",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 152
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h2", {
+    className: "text-color-pink",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 153
+    },
+    __self: this
+  }, "Use diverse frameworks"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 154
+    },
+    __self: this
+  }, " Rokkit.ts is designed to plug in diverse frameworks. Such as HTTP-Server frameworks. The are default adapter which are shipped with the framework. For example default adapters for Express.js and Restify contained within the http-module. Each module could provide several default adapters. The users could also develop their own adapters for any framework they like to.")), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    className: "attribute-text",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 163
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h2", {
+    className: "text-color-pink",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 164
+    },
+    __self: this
+  }, "Polyglot Runtimes"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 165
+    },
+    __self: this
+  }, " Rokkit.ts is originally planned for Node.js, but the upcoming development of Deno, the new TypeScript runtime by Ryan Dahl the creator of Node.js. The project is interesting and should be focused in the near future!")))), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_footer_Footer__WEBPACK_IMPORTED_MODULE_6__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 174
+    },
+    __self: this
+  }));
+}
+
+/* harmony default export */ __webpack_exports__["default"] = (App);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.eaba8fa57bef88af39ab.hot-update.js.map
