3096f0ce800896005f5df47424353e35f9788114
Jann Deterling <jann.deterling@thoughtworks.com>
2020-03-03 13:27:24 +0100

    remoe head from index.js

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,6 @@ import Head from "next/head";
 const Home = () => {
   return (
     <>
-      <Head>
-        <link rel="shortcut icon" href="/static/favicon.ico" />
-        <title>Rokkit.ts - Framework</title>
-      </Head>
       <App />
     </>
   );
