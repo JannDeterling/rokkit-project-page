302683d1edc4bf01cf4f3de9b0e0a92e06cfedd8
Jann Deterling <jann.deterling@thoughtworks.com>
2020-03-03 13:28:52 +0100

    add head from index.js

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,10 @@ import Head from "next/head";
 const Home = () => {
   return (
     <>
+      <Head>
+        <title>Rokkit.ts - Framework</title>
+        <link rel="shortcut icon" href="/static/favicon.ico" />
+      </Head>
       <App />
     </>
   );
