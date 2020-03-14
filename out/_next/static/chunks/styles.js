874357dceff2f2fe142dcf91b36256a804238c68
Jann Deterling <jann.deterling@thoughtworks.com>
2020-03-14 23:29:32 +0100

    Deploy gh-pages

--- out/_next/static/chunks/styles.js
+++ out/_next/static/chunks/styles.js
@@ -177,7 +177,7 @@
           .querySelectorAll("link[href][rel=stylesheet]")
           .forEach(function(link) {
             if (!link.href.match(prefix) || link.stale) return;
-            injectCss(link, link.href.split("?")[0] + "?unix=1584224582913");
+            injectCss(link, link.href.split("?")[0] + "?unix=1584224925276");
           });
       }
     }
